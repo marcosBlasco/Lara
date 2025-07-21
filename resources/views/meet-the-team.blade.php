0b91d5e68cf3d9be576143e3f717b9d7222d1f98
marcosBlasco <marcos.ielect@gmail.com>
2025-07-21 05:00:26 -0300

    day three first component file, and implementation of a very simple nava bar... additionally the web. php file was modified in order to add more urls

---
+++ resources/views/meet-the-team.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>Hello from the Team page.</h1>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/meet-the-team', function () {
+    return view('meet-the-team');
 });

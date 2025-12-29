e9b4fcc6ca1f19c49af2f923d4c95aebfbbc0f89
marcosBlasco <marcos.ielect@gmail.com>
2025-12-28 22:50:21 -0300

    30-days-to-learn-laravel-11/episodes/19. Pro tip #5 Route group

--- routes/web.php
+++ routes/web.php
@@ -9,15 +9,17 @@ Route::redirect('/', '/en');
 Route::view('/es', 'home-es');
 Route::view('/en', 'home-en');
 
-Route::controller(JobController::class)->group(function(){
-    Route::get('/jobs', 'index');
-    Route::get('/jobs/create', 'create');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/{job}', 'show');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
+// Route::controller(JobController::class)->group(function(){
+//     Route::get('/jobs', 'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::post('/jobs', 'store');
+//     Route::get('/jobs/{job}', 'show');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::delete('/jobs/{job}', 'destroy');
+// });
+
+Route::resource('jobs', JobController::class);
 
 Route::view('/about', 'about');
 Route::view('/contact', 'contact'); 

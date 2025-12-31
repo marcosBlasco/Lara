88940424233c0dd5eafb19cd0aec939d65179344
marcosBlasco <marcos.ielect@gmail.com>
2025-12-31 17:46:35 -0300

    fix labels fixed in order to have dark mode suport

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RegisteredUserController extends Controller
 {
@@ -11,6 +13,22 @@ class RegisteredUserController extends Controller
     }
 
     public function store(){
-        dd(request()->all());
+        //validate
+        $validated_attributes = request()->validate([
+            'first_name'    => ['required'],
+            'last_name'     => ['required'],
+            'email'         => ['required', 'email', 'unique:users', 'max:254'],
+            'password' => ['required', 'string', 'min:8', 'confirmed',],
+        ]);
+        //create the user
+        $user = User::create($validated_attributes);
+        //login the user
+
+        Auth::login($user);
+
+        //redirect somewhere
+
+        return redirect('jobs');
+
     }
 }
--- resources/views/components/form-label.blade.php
+++ resources/views/components/form-label.blade.php
@@ -1,1 +1,1 @@
-<label {{ $attributes->merge(['class' => 'block text-sm/6 font-medium text-black dark:text-white']) }}>{{ $slot }}</label>
+<label {{ $attributes->merge(['class' => "block text-sm/6 font-medium text-black dark:text-white"]) }}>{{ $slot }}</label>

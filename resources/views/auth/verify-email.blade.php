7f5dbe6a15e97e97676d863ac3f5c26b2489dce9
marcosBlasco <marcos.ielect@gmail.com>
2026-01-10 07:04:39 -0300

    a better and an easier to see resend verification email button

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -6,17 +6,17 @@
     
 
     <p>
-        Te enviamos un enlace de verificación.
-        Revisá tu correo (y spam).
+        A verification link has been sent to your email address.
+        Please check your inbox (and spam folder).
     </p>
 
     @if (session('status') === 'verification-link-sent')
-        <p>Te reenviamos el enlace.</p>
+        <p>We’ve resent the link.</p>
     @endif
 
     <form method="POST" action="{{ route('verification.send') }}">
         @csrf
-        <button class="bg-gray-950/50 text-gray-300 hover:text-white rounded-md  px-3 py-2 text-sm font-medium"
->Reenviar correo</button>
+        <x-button class="mt-6"
+>Resend email</x-button>
     </form>
 </x-layout>
